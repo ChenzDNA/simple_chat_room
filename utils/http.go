854fd034cb026db3b088fa26964770aa8eb7c0ea
ChenZheng <1007324849@qq.com>
2022-04-26 21:27:04 +0800

    refactor: duplicate fmt.Sprintf

--- utils/http.go
+++ utils/http.go
@@ -3,5 +3,5 @@ package utils
 import "fmt"
 
 func CorsHeader(origin string) string {
-	return fmt.Sprintf(fmt.Sprintf("access-control-allow-credentials: true\r\naccess-control-allow-headers: Origin,Content-Type,Accept,User-Agent,Cookie,Authorization,X-Auth-Token,X-Requested-With\r\naccess-control-allow-methods: GET,PUT,POST,DELETE,PATCH,HEAD,CONNECT,OPTIONS,TRACE\r\naccess-control-allow-origin: %s\r\naccess-control-max-age: 3628800\r\n", origin))
+	return fmt.Sprintf("access-control-allow-credentials: true\r\naccess-control-allow-headers: Origin,Content-Type,Accept,User-Agent,Cookie,Authorization,X-Auth-Token,X-Requested-With\r\naccess-control-allow-methods: GET,PUT,POST,DELETE,PATCH,HEAD,CONNECT,OPTIONS,TRACE\r\naccess-control-allow-origin: %s\r\naccess-control-max-age: 3628800\r\n", origin)
 }
